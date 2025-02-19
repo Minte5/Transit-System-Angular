8e9775a17e5e8bed68b825d4b2143443033263c6
Mintesnot <mintesnotbelete7@gmail.com>
2025-02-19 12:34:31 +0300

    The project's dependencies are setted

---
+++ transit-system/src/app/transit/transit.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TransitRoutingModule } from './transit-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TransitRoutingModule
+  ]
+})
+export class TransitModule { }
