ae8d50c9c0fa3cd1012e4fa41b5ada2a2acd5b47
Mintesnot <mintesnotbelete7@gmail.com>
2025-02-19 12:45:49 +0300

    Routes are defines and item service is set

--- transit-system/src/app/services/item.service.ts
+++ transit-system/src/app/services/item.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
+  private apiUrl = 'https://your-api-url.com/api/items';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getReceivedItems(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/received`);
+  }
+
+  receiveItem(item: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/receive`, item);
+  }
+
+  sendForInspection(itemId: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/send-for-inspection/${itemId}`, {});
+  }
+
+  getInspectionItems(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/inspection`);
+  }
+
+  receiveFromInspection(itemId: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/receive-from-inspection/${itemId}`, {});
+  }
+
+  sendToStore(item: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/send-to-store`, item);
+  }
+
+  getSentToStoreItems(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/sent-to-store`);
+  }
 }
--- transit-system/src/app/transit/transit-routing.module.ts
+++ transit-system/src/app/transit/transit-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReceiveItemComponent } from './receive-item/receive-item.component';
+import { ReceivedItemsListComponent } from './received-items-list/received-items-list.component';
+import { InspectionItemsListComponent } from './inspection-items-list/inspection-items-list.component';
+import { SendToStoreComponent } from './send-to-store/send-to-store.component';
+import { SentToStoreListComponent } from './sent-to-store-list/sent-to-store-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'receive-item', component: ReceiveItemComponent },
+  { path: 'received-items', component: ReceivedItemsListComponent },
+  { path: 'inspection-items', component: InspectionItemsListComponent },
+  { path: 'send-to-store', component: SendToStoreComponent },
+  { path: 'sent-to-store', component: SentToStoreListComponent },
+  { path: '', redirectTo: 'received-items', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
