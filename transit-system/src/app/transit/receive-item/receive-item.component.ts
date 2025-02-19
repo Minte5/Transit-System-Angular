65b06e01d0427f5df6c19d442605252d12ebc03c
Mintesnot <mintesnotbelete7@gmail.com>
2025-02-19 13:11:18 +0300

    Receive item form is set

--- transit-system/src/app/transit/receive-item/receive-item.component.ts
+++ transit-system/src/app/transit/receive-item/receive-item.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ItemService } from '../../services/item.service';
+import { Router } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-receive-item',
   standalone: true,
-  imports: [],
+  imports: [BrowserModule,
+      FormsModule,],
   templateUrl: './receive-item.component.html',
-  styleUrl: './receive-item.component.css'
+  styleUrls: ['./receive-item.component.css']
 })
 export class ReceiveItemComponent {
+  item = {
+    itemName: '',
+    supplier: '',
+    quantity: 0
+  };
 
+  constructor(private itemService: ItemService, private router: Router) { }
+
+  receiveItem() {
+    this.itemService.receiveItem(this.item).subscribe({
+      next: () => {
+        alert('Item received successfully');
+        this.router.navigate(['/transit/received-items']);
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Error receiving item');
+      }
+    });
+  }
 }
+
--- transit-system/src/app/transit/transit.module.ts
+++ transit-system/src/app/transit/transit.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TransitRoutingModule } from './transit-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    TransitRoutingModule
+    TransitRoutingModule,
+    BrowserModule,
+    FormsModule,
   ]
 })
 export class TransitModule { }
